543852f4c44177d1950e7920067a6d963f5ed8c4
dkuhl2002-alt <d.kuhl2002@gmail.com>
2026-08-26 23:26:21 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C="kuhlfamily-v4";
-const A=["./","./index.html","./styles.css","./app.js","./firebase-config.js","./assets/family.jpg"];
+const C="kuhlfamily-v6";
+const A=["./","./index.html","./styles.css?v=6","./app.js?v=6","./firebase-config.js","./assets/family.jpg"];
 
 self.addEventListener("install",e=>{
   self.skipWaiting();
